70294d7dbd75f8c5bd905422fe998579b56084f5
RafaelEmery <rafael.emerycade@gmail.com>
2020-07-27 21:52:13 -0300

    First commit. End of Class 1

---
+++ src/components/Footer/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="https://www.alura.com.br/">
+        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      </a>
+      <p>
+        Feito por 
+        {' '}
+        <a href="https://rafaelemery.github.io">
+          Rafael Emery
+        </a>
+        {' '}
+        durante a
+        {' '}
+        <a href="https://www.alura.com.br/">
+          Imersão React da Alura
+        </a>
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
